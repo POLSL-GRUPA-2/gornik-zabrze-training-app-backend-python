148b93dae84ff4500eda2ee0f37d8d28415ff3d2
Jeremi Kot <43934790+jekot1234@users.noreply.github.com>
2021-05-21 18:56:41 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://szef:rootroot@localhost/gornik"
+
+dataBase = SQLAlchemy(app)
+
+class Users(dataBase.Model):
+    id = dataBase.Column("id", dataBase.Integer, primary_key=True)
+    fistName = dataBase.Column("firstname", dataBase.String(100))
+    lastName = dataBase.Column("lastname", dataBase.String(100))
+    email = dataBase.Column("email", dataBase.String(100))
+
+    def __init__(self, firstName, lastName, email):
+        self.fistName = firstName
+        self.lastName = lastName
+        self.email = email
+
+
+
+class HelloWorld(Resource):
+    def get(self):
+
+        us = Users.query.filter_by(fistName="Jakub").first()
+
+        if us:
+            return us.lastName
+        else:
+            return "prrrrt"
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    dataBase.create_all()
+    app.run(debug=True)
