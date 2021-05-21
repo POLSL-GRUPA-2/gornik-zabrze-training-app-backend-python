12bb1b8bbb991dc4db354f9403a031f81741ae3e
Jeremi Kot <43934790+jekot1234@users.noreply.github.com>
2021-05-21 22:15:40 +0200

    Fix entries generator

--- utilities/database_entries_generator.py
+++ utilities/database_entries_generator.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import time
+import string
 
 if len(sys.argv) != 3:
     print("Two arguments needed: table name and number of entries")
@@ -21,7 +22,7 @@ if sys.argv[1] == "users":
         name = names[random.randrange(len(names))]
         lastname = lastnames[random.randrange(len(lastnames))]
         email = name[0:4].lower() + lastname[0:3].lower() + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + "@studnet.polsl.pl"
-        password = str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9))
+        password = ''.join(random.sample(string.ascii_lowercase, 12))
 
         query = "insert into {0} (first_name, last_name, email, password_hash) values ('{1}', '{2}', '{3}', '{4}');".format(sys.argv[1], name, lastname, email, password)
 
