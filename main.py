edfeab627bb6c45e45f6414b889810641cab0c25
Jeremi Kot <43934790+jekot1234@users.noreply.github.com>
2021-05-23 17:08:33 +0200

    Add registration, login, authentication

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, json, request, jsonify
+from flask.helpers import make_response
+from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 import xml.etree.ElementTree as XMLconfig
-from flask_restful import reqparse
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 XMLtree = XMLconfig.parse('config.xml')
 XMLroot = XMLtree.getroot()
@@ -13,6 +17,7 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dataBaseConfig.find('user').text,
  dataBaseConfig.find('password').text, dataBaseConfig.find('host').text, dataBaseConfig.find('name').text )
+app.config['SECRET_KEY'] = "EEAAA"
 
 dataBase = SQLAlchemy(app)
 
@@ -21,15 +26,18 @@ class Serializable():
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class Users(dataBase.Model, Serializable):
-    id = dataBase.Column("id", dataBase.Integer, primary_key=True)
+    id = dataBase.Column("id", dataBase.String(100), primary_key=True)
     first_name = dataBase.Column("first_name", dataBase.String(100))
     last_name = dataBase.Column("last_name", dataBase.String(100))
     email = dataBase.Column("email", dataBase.String(100))
+    password_hash = dataBase.Column('password_hash', dataBase.String(64))
 
-    def __init__(self, first_name, last_name, email):
+    def __init__(self, id, first_name, last_name, email, password_hash):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.password_hash = password_hash
 
     parser = reqparse.RequestParser()
     parser.add_argument('id', type=int, help='Id cannot be converted')
@@ -69,7 +77,47 @@ class UserRes(Resource):
     def delete(self):
         return "delete"
 
+class Login(Resource):
+    def get(self):
+        
+        auth = request.authorization
+        if not auth or not auth.username or not auth.password:
+            return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+        user = Users.query.filter_by(email=auth.username).first()
+
+        if not user:
+            return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+        if check_password_hash(user.password_hash, auth.password):
+            token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config['SECRET_KEY'])
+
+            return jsonify({'token' : token})
+        
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+
+        #chceck if email already exists
+        users_email = Users.query.filter_by(email=data['email']).first()
+        if users_email:
+            return jsonify({'message' : 'User with provided email address already exist!'})
+
+        hashed_password = generate_password_hash(data['password'], method='sha256')
+
+        new_user = Users(id=str(uuid.uuid1()), first_name=data['first_name'], last_name=data['last_name'], email=data['email'], password_hash=hashed_password)
+        dataBase.session.add(new_user)
+        dataBase.session.commit()
+
+        return jsonify({'message' : 'User created successfully'})
+        
+
+
 api.add_resource(UserRes, '/user')
+api.add_resource(Login, '/login')
+api.add_resource(Register, '/register')
 
 if __name__ == '__main__':
     dataBase.create_all()
