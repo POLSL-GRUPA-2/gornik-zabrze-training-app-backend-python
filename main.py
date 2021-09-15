e17a4ca7c8b7d13a7e63e3e28c2fdb5f4aef3e00
Jeremi Kot <43934790+jekot1234@users.noreply.github.com>
2021-09-16 01:50:34 +0200

    Team message functionality added fixes

--- main.py
+++ main.py
@@ -738,7 +738,7 @@ api.add_resource(Check_role, '/role')
 def main(*args, **kwargs):
     dataBase.create_all()
     #port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='localhost')
+    #app.run(debug=True, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
